7ba0e5dc85fffdb7730f08484b4c2c00317b79b2
Shingo <shingo0620@gmail.com>
2022-05-17 13:44:47 +0800

    Adjust the pos and rot interface

--- src/components/TraitSelector.vue
+++ src/components/TraitSelector.vue
@@ -11,6 +11,25 @@
 
     <div v-if="isExpended" class="w-full px-2 pt-4 pb-2 bg-gray-700">
       <!-- Positions -->
+      <div class="flex items-center w-full gap-2 mt-2">
+        <label class="uppercase whitespace-nowrap">x-pos</label>
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['x-position']" type="range" class="w-full">
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['x-position']" type="number" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded">
+      </div>
+
+      <div class="flex items-center w-full gap-2 mt-2">
+        <label class="uppercase whitespace-nowrap">y-pos</label>
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['y-position']" type="range" class="w-full">
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['y-position']" type="number" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded">
+      </div>
+
+      <div class="flex items-center w-full gap-2 mt-2">
+        <label class="uppercase whitespace-nowrap">z-pos</label>
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['z-position']" type="range" class="w-full">
+        <input min="-2" max="2" step="0.00001" v-model.number="properties['z-position']" type="number" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded">
+      </div>
+
+      <!--
       <div class="flex gap-2">
         <div class="relative w-full">
           <label :for="`${properties.name}-x-pos`" class="absolute px-1 text-xs text-black border border-black rounded pointer-events-none left-2 -top-2 bg-gray-50">X-POS</label>
@@ -27,22 +46,23 @@
           <input :id="`${properties.name}-z-pos`" v-model.number="properties['z-position']" type="number" class="w-full px-1 pt-2 pb-1 text-center text-black border-2 rounded border-primary bg-gray-50">
         </div>
       </div>
+      -->
 
       <!-- Rotations -->
       <div class="flex items-center w-full gap-2 mt-2">
         <label class="uppercase whitespace-nowrap" :for="`${properties.name}-x-rot`">x-rot</label>
         <input min="-360" max="360" v-model.number="properties['x-rotate']" class="w-full" :id="`${properties.name}-x-rot`" type="range">
-        <input min="-360" max="360" v-model.number="properties['x-rotate']" class="w-12 p-1 text-xs text-center text-black rounded" type="number">
+        <input min="-360" max="360" v-model.number="properties['x-rotate']" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded" type="number">
       </div>
       <div class="flex items-center w-full gap-2 mt-2">
         <label class="uppercase whitespace-nowrap" :for="`${properties.name}-y-rot`">y-rot</label>
         <input min="-360" max="360" v-model.number="properties['y-rotate']" class="w-full" :id="`${properties.name}-y-rot`" type="range">
-        <input min="-360" max="360" v-model.number="properties['y-rotate']" class="w-12 p-1 text-xs text-center text-black rounded" type="number">
+        <input min="-360" max="360" v-model.number="properties['y-rotate']" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded" type="number">
       </div>
       <div class="flex items-center w-full gap-2 mt-2">
         <label class="uppercase whitespace-nowrap" :for="`${properties.name}-z-rot`">z-rot</label>
         <input min="-360" max="360" v-model.number="properties['z-rotate']" class="w-full" :id="`${properties.name}-z-rot`" type="range">
-        <input min="-360" max="360" v-model.number="properties['z-rotate']" class="w-12 p-1 text-xs text-center text-black rounded" type="number">
+        <input min="-360" max="360" v-model.number="properties['z-rotate']" class="shrink-0 w-[56px] p-1 text-xs text-center text-black rounded" type="number">
       </div>
     </div>
   </div>
